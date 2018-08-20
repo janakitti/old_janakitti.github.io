2bb53265a34111a6df37c34d8ab2237b7846461d
janakitti <janakittiratana@gmail.com>
2018-08-19 23:35:57 -0400

    Added resume feature

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 $('.parallax-window').parallax({imageSrc: ''});
+
+function displayMessage() {
+    var messages = ["Hey there!",
+                    "It's pronnounced ya-na-kit",
+                    "This message was pre-programmed",
+                    "I like polar bears",
+                    "(It's a Thai name)",
+                    "Wow!",
+                    "Creator &#9679; Designer &#9679; Developer"]
+    var num = Math.floor(Math.random() * (messages.length - 1 - 0) + 0);
+    document.getElementById("textToChange").innerHTML = messages[num];
+}
