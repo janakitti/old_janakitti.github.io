9640cd13cd910ee62d6021eaef7a9d6b4e7936c0
janakitti <janakittiratana@gmail.com>
2019-06-05 13:18:21 -0400

    major update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,16 @@
 $('.parallax-window').parallax({imageSrc: ''});
 
 function displayMessage() {
-    var messages = ["Hey there!",
-                    "It's pronnounced ya-na-kit",
-                    "This message was pre-programmed",
+    var messages = ["Hello",
+                    "Hello",
+                    "Hello",
                     "I like polar bears",
-                    "(It's a Thai name)",
-                    "Wow!",
+                    "Hello",
+                    "Press Refresh",
                     "Creator &#9679; Designer &#9679; Developer"]
+
+
     var num = Math.floor(Math.random() * (messages.length - 1 - 0) + 0);
     document.getElementById("textToChange").innerHTML = messages[num];
+
 }
